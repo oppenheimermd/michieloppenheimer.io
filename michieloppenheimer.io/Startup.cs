4f1f3cc3bc0f64ce899656ceeb8f40ed28b47189
Michiel <michieloppenheimer@gmail.com>
2018-10-14 20:14:18 +0100

    website ui added

---
+++ michieloppenheimer.io/Startup.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using WebEssentials.AspNetCore.OutputCaching;
+using WebMarkupMin.AspNetCore2;
+using WebMarkupMin.Core;
+using WilderMinds.MetaWeblog;
+
+using IWmmLogger = WebMarkupMin.Core.Loggers.ILogger;
+//using MetaWeblogService = WFC.Web.Services.MetaWeblogService;
+using WmmNullLogger = WebMarkupMin.Core.Loggers.NullLogger;
+
+namespace michieloppenheimer.io
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(a => a.AddServerHeader = false)
+                .Build();
+
+        public IConfiguration Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //services.AddSingleton<IUserServices, BlogUserServices>();
+            //services.AddSingleton<IBlogService, FileBlogService>();
+            //services.AddSingleton<IEquipmentService, FileEquipmentService>();
+            //services.Configure<BlogSettings>(Configuration.GetSection("blog"));
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //services.AddMetaWeblog<MetaWeblogService>();
+
+
+            // Progressive Web Apps https://github.com/madskristensen/WebEssentials.AspNetCore.ServiceWorker
+            services.AddProgressiveWebApp(new WebEssentials.AspNetCore.Pwa.PwaOptions
+            {
+                OfflineRoute = "/shared/offline/"
+            });
+
+            // Output caching (https://github.com/madskristensen/WebEssentials.AspNetCore.OutputCaching)
+            services.AddOutputCaching(options =>
+            {
+                options.Profiles["default"] = new OutputCacheProfile
+                {
+                    Duration = 3600
+                };
+            });
+
+            // Cookie authentication.
+            services
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login/";
+                    options.LogoutPath = "/logout/";
+                });
+
+            // HTML minification (https://github.com/Taritsyn/WebMarkupMin)
+            services
+                .AddWebMarkupMin(options =>
+                {
+                    options.AllowMinificationInDevelopmentEnvironment = true;
+                    options.DisablePoweredByHttpHeaders = true;
+                })
+                .AddHtmlMinification(options =>
+                {
+                    options.MinificationSettings.RemoveOptionalEndTags = false;
+                    options.MinificationSettings.WhitespaceMinificationMode = WhitespaceMinificationMode.Safe;
+                });
+
+            services.AddSingleton<IWmmLogger, WmmNullLogger>(); // Used by HTML minifier
+
+            //  Using NPM
+            // Bundling, minification and Sass transpilation (https://github.com/ligershark/WebOptimizer)
+            services.AddWebOptimizer(pipeline =>
+            {
+                pipeline.MinifyJsFiles();
+                /*pipeline.CompileScssFiles()
+                        .InlineImages(1);*/
+            });
+
+            //  Enforce HTTPS
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
+            //  https://www.softfluent.com/blog/dev/Enabling-gzip-compression-with-ASP-NET-Core
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+
+
+            services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = new[]
+                {
+                    // Default
+                    "text/plain",
+                    "text/css",
+                    "application/javascript",
+                    "text/html",
+                    "application/xml",
+                    "text/xml",
+                    "application/json",
+                    "text/json",
+                    // Custom
+                    "image/svg+xml"
+                };
+            });
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                //app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Shared/Error");
+            }
+
+            app.Use((context, next) =>
+            {
+                context.Response.Headers["X-Content-Type-Options"] = "nosniff";
+                if (context.Request.IsHttps)
+                {
+                    context.Response.Headers["Strict-Transport-Security"] = "max-age=63072000; includeSubDomains";
+                }
+                return next();
+            });
+
+            app.UseStatusCodePagesWithReExecute("/Shared/Error");
+            app.UseWebOptimizer();
+
+            app.UseStaticFilesWithCache();
+
+            if (Configuration.GetValue<bool>("forcessl"))
+            {
+                app.UseRewriter(new RewriteOptions().AddRedirectToHttps());
+            }
+
+            //app.UseMetaWeblog("/metaweblog");
+            app.UseAuthentication();
+
+            app.UseOutputCaching();
+            app.UseWebMarkupMin();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Blog}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
