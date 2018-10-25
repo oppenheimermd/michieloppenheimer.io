f1d025a508c66b072c92c4b2aea0f07a5272dcd4
Michiel <michieloppenheimer@gmail.com>
2018-10-25 16:57:34 +0100

    app cleanup / models updated / logic updated

---
+++ michieloppenheimer.io/Controllers/SharedController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// ReSharper disable once CheckNamespace
+namespace Web.Controllers
+{
+    public class SharedController : Controller
+    {
+        public IActionResult Error()
+        {
+            return View(Response.StatusCode);
+        }
+
+        /// <summary>
+        ///  This is for use in wwwroot/serviceworker.js to support offline scenarios
+        /// </summary>
+        public IActionResult Offline()
+        {
+            return View();
+        }
+    }
+}
---
+++ michieloppenheimer.io/Models/Helpers/DefaultPostComparer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+// ReSharper disable once CheckNamespace
+namespace Blog.Models.Helpers
+{
+    /// <summary>
+    /// .NET doesn’t automatically know how to compare the <see cref="Post"/> objects in a way that is useful
+    /// for our program. Instead, the comparison will be based on reference equality.  As such we need to specify own
+    /// equality comparer.
+    /// </summary>
+    public class DefaultPostComparer : IEqualityComparer<Post>
+    {
+        public bool Equals(Post x, Post y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(Post obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
